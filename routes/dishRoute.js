34b23d70446b6e987b3c8e8720f995c996432ed3
siamak <Siamak.nikefar@gmail.com>
2020-07-26 17:28:45 +0430

    first assaignment(task1) : setting up '/dishes/dishId' route

--- routes/dishRoute.js
+++ routes/dishRoute.js
@@ -26,4 +26,30 @@ dishRouter.route('/')
     res.end('Dleting all the dishes !!!')
 })
 
+
+//'/dishes/:dishId' route
+dishRouter.route('/:dishId')
+.all((req, res, next) => {
+    res.statusCode = 200
+    res.setHeader('Content-Type', 'text/plain')
+    next()
+})
+.get((req, res) => {
+    res.write(`will send you information about dish number ${req.params.dishId} \n`)
+    res.end(`Name of the dish you requested for info is : ${req.body.name} and its description is : ${req.body.description} `)
+})
+.post((req, res) => {
+    res.statusCode = 403
+    res.send(`POST method nost supported on /dishes/${req.params.dishId}`)
+})
+.put((req, res) => {
+    res.write(`will modify dish number ${req.params.dishId} information \n`)
+    res.end(`The dish has been changed to :
+    name: ${req.body.name}
+    description : ${req.body.description} `)
+})
+.delete((req, res) => {
+    res.end(`Will delete dish number ${req.params.dishId} from the dishes`)
+})
+
 module.exports = dishRouter
